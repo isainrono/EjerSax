57222b42bb5834de4b6a8e2eab393456f4d72af2
alu2017440 <alu2017440@a32o23.aulas.local>
2018-11-07 18:39:09 +0100

    empezamos2

--- src/ejersax/EjerSax.java
+++ src/ejersax/EjerSax.java
@@ -15,7 +15,7 @@ public class EjerSax {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("empezamos");
     }
     
 }
