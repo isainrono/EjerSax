768495941e9a8a3ee4050c244e033c032ae8a3cc
alu2017440 <alu2017440@a32o23.aulas.local>
2018-11-07 20:19:21 +0100

    bien

---
+++ src/Model/Arbol.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author alu2017440
+ */
+public class Arbol {
+    
+    private String elementName;
+    
+    public Arbol(){
+    }
+
+
+    public String getElementName() {
+        return elementName;
+    }
+
+    public void setElementName(String elementName) {
+        this.elementName = elementName;
+    }
+    
+    
+    
+}
--- src/ejersax/EjerSax.java
+++ src/ejersax/EjerSax.java
@@ -5,6 +5,10 @@
  */
 package ejersax;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author alu2017440
@@ -15,7 +19,36 @@ public class EjerSax {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("empezamos");
+        boolean salir = false;
+        
+        do{
+            try {
+                int option = 0;
+                Templates.menu();
+                
+                option = Control.pedirEntero("Que quiere hacer?");
+                
+                switch(option){
+                    case 1:
+                        System.out.println("Estructura del fichero");
+                        Templates.saxParser();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        salir = true;
+                        break;
+                        
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(EjerSax.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }while(!salir);
     }
     
 }
